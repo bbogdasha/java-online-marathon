f6544e41db039f9ca68cf4f0f5b1d312ef0adfa1
bogdan <bogdan4888@gmail.com>
2021-01-18 23:52:34 +0200

    test in progress

--- sprint19/Spring REST Lesson Two/src/test/java/com/bogdan/controller/StudentControllerTest.java
+++ sprint19/Spring REST Lesson Two/src/test/java/com/bogdan/controller/StudentControllerTest.java
@@ -3,10 +3,15 @@ package com.bogdan.controller;
 import com.bogdan.model.Course;
 import com.bogdan.model.Student;
 import com.bogdan.service.StudentServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,22 +28,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-@RunWith(SpringRunner.class)
-//Spring предоставляет соответствующие аннотации: @MockBean @SpyBean
-@WebMvcTest(value = StudentController.class)
-//@ WebMvcTest сообщит Spring Boot о создании экземпляра только веб-слоя, а не всего контекста.
-//Из-за этого тесты контроллера, использующие _ @ WebMvcTest _ , будут выполняться быстрее,
-// чем при других подходах .
-@WithMockUser
-//Поскольку MockMvc настроен для нас, мы можем использовать @ WithMockUser для наших
-// тестов без какой-либо дополнительной настройки
+@RunWith(MockitoJUnitRunner.class)
 public class StudentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private StudentServiceImpl studentService;
+
+    @InjectMocks
+    StudentController studentController = new StudentController(studentService);
 
     String exampleCourseJson = "{\"name\":\"Spring\",\"info\":\"10Steps\",\"steps\":[]}";
     Course course = new Course("Spring", "Spring info", new ArrayList<>());
@@ -48,45 +48,52 @@ public class StudentControllerTest {
 
     ObjectMapper mapper = new ObjectMapper();
 
-    @Test
-    public void createStudentTest() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/students/add")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(exampleStudentPojo));
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        MockHttpServletResponse response = result.getResponse();
-        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+    @Before
+    public void init() {
+        System.out.println("++++");
+        studentController.create(exampleStudentPojo);
     }
 
-    @Test
-    public void createCourseTest() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/students/1/courses/add")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(exampleStudentPojo));
+//    @Test
+//    public void createStudentTest() throws Exception {
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders
+//                .post("/students/add")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(mapper.writeValueAsString(exampleStudentPojo));
+//
+//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+//        MockHttpServletResponse response = result.getResponse();
+//        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+//    }
 
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        MockHttpServletResponse response = result.getResponse();
-        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-    }
-
+//    @Test
+//    public void createCourseTest() throws Exception {
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders
+//                .post("/students/1/courses/add")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(mapper.writeValueAsString(exampleStudentPojo));
+//
+//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+//        MockHttpServletResponse response = result.getResponse();
+//        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+//    }
+//
     @Test
     public void retrieveDetailsForCourse() throws Exception {
-        RequestBuilder requestBuilderGet = MockMvcRequestBuilders
-                .get("/students/1")
-                .accept(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilderGet).andReturn();
-
-        String expected = "{id:1,name:Nick,info:Nick info,courses:[]}";
-
-        String json = result.getResponse().getContentAsString();
-        System.out.println("JSON: " + json);
-        System.out.println("STRING: " + expected);
+//        RequestBuilder requestBuilderGet = MockMvcRequestBuilders
+//                .get("/students/1")
+//                .accept(MediaType.APPLICATION_JSON);
+//
+//
+//        MvcResult result = mockMvc.perform(requestBuilderGet).andReturn();
+//
+//        String expected = "{id:1,name:Nick,info:Nick info,courses:[]}";
+//
+//        String json = result.getResponse().getContentAsString();
+//        System.out.println("JSON: " + json);
+//        System.out.println("STRING: " + expected);
 
 //        JSONAssert.assertEquals(expected,
 //                result.getResponse().getContentAsString(), false);
